02c91012f0d2be3a9dddb7e81d4be5810d7bec9d
fazalhashmii <hashmifazal34@gmail.com>
2026-08-24 03:46:20 +0530

    Calculator program samples - number formatting using commas

--- calculator.py
+++ calculator.py
@@ -1,13 +1,19 @@
-#rounding off the output to the nearest integer
+#number formatting using commas
 
 #method 1
 x = float(input("x= "))
 y = float(input("y= "))
 z = round(x+y)
-print (z)
+print (f"{z:,}") # ":" applies a formatting rule to z and "," adds commas as thousands separators
 
 #method 2
 x = float(input("x= "))
 y = float(input("y= "))
-print (round(x+y))
+print (f"{round(x+y):,}")
 
+# Think of it like this: 
+# f"{variable:format}"
+# {z} puts the value of z
+# : start formatting instructions
+# , format the numbner with commas
+# f"{z:,}" = Put z here, and display it with commas
