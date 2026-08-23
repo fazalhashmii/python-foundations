8c868851766030d0fd0d2a59ee91253d7d6c75ff
fazalhashmii <hashmifazal34@gmail.com>
2026-08-24 02:17:19 +0530

    Update string practice - title function

--- string.py
+++ string.py
@@ -1,5 +1,5 @@
-#capitalize makes the first char of the str uppercase and rest lower case
+#title mcapitalize the first letter of every word in a str
 name = input ("What's ya name? ")
 name = name.strip()
-name = name.capitalize()
+name = name.title() #capitalize works only on the first word of the str and not on every word
 print (f"Hello, {name}")
