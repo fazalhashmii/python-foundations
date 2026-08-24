a9fa9cdd2821fb5a37bd0a14da782134462c2533
fazalhashmii <hashmifazal34@gmail.com>
2026-08-25 02:05:14 +0530

    Update def functions - Print in same line using hello call

--- def.py
+++ def.py
@@ -1,14 +1,7 @@
-# def is used to define (create) a function.
+#print in the same line
 
-# program 1: no def used for hello; thus we shall see the error when the program is executed
+def hello (x):
+    print("Hello,", x)
 
-
-
-# program 2: creating a hello function to perform the program 1 correctly
-
-def hello(): # ":" tells python that the function’s code block starts here
-    print ("Hello") # this belongs to the defined function
-
-name = input ("What is your name? ")
-hello ()
-print (name)
+name = input ("Wahts your name? ")
+hello (name)
